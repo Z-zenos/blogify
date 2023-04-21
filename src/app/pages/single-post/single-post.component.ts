dc44b4927a6b77e9ac2d3666925d9c442eff7ec5
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2023-04-22 03:53:42 +0700

    [Update]: add fixed class to sidebar, readign time

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
   styleUrls: ['./single-post.component.scss']
 })
-export class SinglePostComponent implements OnInit {
+export class SinglePostComponent implements OnInit, AfterViewInit {
+  @ViewChild('content', { static: false }) contentEl?: ElementRef;
 
-  constructor() { }
+  readingTime: number = 0;
+
+  constructor(private _cdref: ChangeDetectorRef) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.caculateTimeReading();
+    this._cdref.detectChanges();
+  }
+
+  caculateTimeReading() {
+    const text = this.contentEl?.nativeElement.innerText;
+    const wpm = 225;
+    const words = text.trim().split(/\s+/).length;
+    const time = Math.ceil(words / wpm);
+    this.readingTime = time;
   }
 
 }
