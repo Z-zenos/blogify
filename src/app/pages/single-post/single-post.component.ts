d0590a750ebf004054034acf7db88ae424745b2c
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2023-04-22 12:34:01 +0700

    [Update]: add sidebar heading, tool icon, awards icon

--- src/app/pages/single-post/single-post.component.ts
+++ src/app/pages/single-post/single-post.component.ts
@@ -3,12 +3,18 @@ import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewCh
 @Component({
   selector: 'app-single-post',
   templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.scss']
+  styleUrls: ['./single-post.component.scss'],
 })
 export class SinglePostComponent implements OnInit, AfterViewInit {
   @ViewChild('content', { static: false }) contentEl?: ElementRef;
 
   readingTime: number = 0;
+  bookmarked: boolean = false;
+  clapped: boolean = false;
+  hearted: boolean = false;
+  stared: boolean = false;
+  lighted: boolean = false;
+  moneyed: boolean = false;
 
   constructor(private _cdref: ChangeDetectorRef) { }
 
@@ -20,12 +26,40 @@ export class SinglePostComponent implements OnInit, AfterViewInit {
     this._cdref.detectChanges();
   }
 
-  caculateTimeReading() {
+  caculateTimeReading(): void {
     const text = this.contentEl?.nativeElement.innerText;
     const wpm = 225;
     const words = text.trim().split(/\s+/).length;
     const time = Math.ceil(words / wpm);
     this.readingTime = time;
+  }
+
+  toggle(typeIcon: string): void {
+    switch (typeIcon) {
+      case 'bookmark':
+        this.bookmarked = !this.bookmarked;
+        break;
+
+      case 'clap':
+        this.clapped = !this.clapped;
+        break;
+
+      case 'heart':
+        this.hearted = !this.hearted;
+        break;
+
+      case 'money':
+        this.moneyed = !this.moneyed;
+        break;
+
+      case 'star':
+        this.stared = !this.stared;
+        break;
+
+      case 'light':
+        this.lighted = !this.lighted;
+        break;
+    }
   }
 
 }
