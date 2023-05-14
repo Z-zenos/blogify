8f3e60fd4d4111411d85ccd57c57cb3c0be04f41
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2023-05-14 18:02:20 +0700

    [Update]: auto close after print

--- src/app/pages/print-post/print-post.component.ts
+++ src/app/pages/print-post/print-post.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs';
 import { IPost } from 'src/app/models/post.interface';
 import { PostService } from 'src/app/services/post.service';
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'blog-print-post',
@@ -12,21 +13,34 @@ import { PostService } from 'src/app/services/post.service';
 export class PrintPostComponent implements OnInit {
   post?: IPost;
   readingTime: number = 0;
+  title: string = '';
 
   constructor(
     private _activatedRoute: ActivatedRoute,
     private _postService: PostService,
-  ) { }
+    private _titleService: Title
+  ) { 
+
+  }
 
   ngOnInit(): void {
     this._activatedRoute.queryParams
     .pipe(
-      switchMap(query => this._postService.getPostByPermalink(query['permalink']))
+      switchMap(query => {
+        this.title = query['permalink'];
+        this._titleService.setTitle(this.title);
+        return this._postService.getPostByPermalink(query['permalink'])
+      })
     )
     .subscribe((post: any) => {
       [this.post] = post;
       this.caculateTimeReading();
-      setTimeout(() => window.print(), 2500);
+
+      setTimeout(() => {
+        window.print();
+        window.close();
+
+      }, 2500);
     });
   }
 
